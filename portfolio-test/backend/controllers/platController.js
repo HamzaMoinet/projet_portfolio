5eec9c160e33acbef3a76c8c9a0d3633b788a166
HamzaMoinet <moinethamza0@gmail.com>
2025-06-10 15:59:14 +0200

    mettre les nom des ingredients à la place de l'id

--- portfolio-test/backend/controllers/platController.js
+++ portfolio-test/backend/controllers/platController.js
@@ -1,10 +1,17 @@
 const Plat = require('../models/Plat');
+const Ingredient = require('../models/Ingredient');
 
 // Créer un plat
 exports.createPlat = async (req, res) => {
   try {
     const { name, price, ingredients } = req.body;
-    const plat = new Plat({ name, price, ingredients });
+    // Si ingredients contient des noms, on les convertit en IDs
+    const ingredientDocs = await Ingredient.find({ name: { $in: ingredients } });
+    if (ingredientDocs.length !== ingredients.length) {
+      return res.status(400).json({ error: "Un ou plusieurs ingrédients n'existent pas." });
+    }
+    const ingredientIds = ingredientDocs.map(ing => ing._id);
+    const plat = new Plat({ name, price, ingredients: ingredientIds });
     await plat.save();
     res.status(201).json(plat);
   } catch (err) {
@@ -15,7 +22,7 @@ exports.createPlat = async (req, res) => {
 // Obtenir tous les plats
 exports.getPlats = async (req, res) => {
   try {
-    const plats = await Plat.find();
+    const plats = await Plat.find().populate('ingredients');
     res.json(plats);
   } catch (err) {
     res.status(500).json({ error: err.message });
