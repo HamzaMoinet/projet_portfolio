98dd1f2c2f4d48df74adfb5220d6a27405f0750a
HamzaMoinet <moinethamza0@gmail.com>
2025-06-11 18:20:16 +0200

    add router for put

--- portfolio-test/backend/routes/plats.js
+++ portfolio-test/backend/routes/plats.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { createPlat, getPlats, deletePlat, putPlat } = require('../controllers/platController');
 
 router.post('/', createPlat); // Créer un plat
-router.get('/', getPlats); //Lister tous les plats
+router.get('/', getPlats); // Lister tous les plats
+router.delete('/:id', deletePlat); // Supprimer un plat
+router.put('/:id', putPlat);
 
 
 module.exports = router;
