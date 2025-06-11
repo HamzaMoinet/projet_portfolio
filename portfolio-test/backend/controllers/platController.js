b207b3ebe85700239d475193322eea764bf48e2c
HamzaMoinet <moinethamza0@gmail.com>
2025-06-11 18:19:26 +0200

    add fonction delete and put

--- portfolio-test/backend/controllers/platController.js
+++ portfolio-test/backend/controllers/platController.js
@@ -28,3 +28,51 @@ exports.getPlats = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// Suprimer un plat
+exports.deletePlat = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const plat = await Plat.findByIdAndDelete(id);
+    if (!plat) {
+      return res.status(404).json({ error: 'Plat non trouvé.' })
+    }
+    res.json({ message: 'Plat supprimé avec succès.' })
+  } catch (err) {
+    res.status(500).json({ error: err.message})
+  }
+};
+
+
+// Modifier un plat
+exports.putPlat = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, ingredients } = req.body;
+    const ingredientDocs = await Ingredient.find({ name: { $in: ingredients } });
+    if (ingredientDocs.length !== ingredients.length) {
+      return res.status(400).json({ error: "Un ou plusieurs ingrédients n'existent pas." });
+    }
+    const ingredientIds = ingredientDocs.map(ing => ing._id);
+    const plat = await Plat.findByIdAndUpdate(id, { name, price, ingredients: ingredientIds }, { new: true });
+    if (!plat) {
+      return res.status(404).json({ error: 'Plat non trouvé.' });
+    }
+    res.json(plat);
+  }
+  catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+/* test crud plat
+{
+  "name": "Nom du plat",
+  "price": 12.99,
+  "ingredients": [
+    "nom de l'ingrédient 1",
+    "nom de l'ingrédient 2",
+    ...
+  ]
+}
+*/
