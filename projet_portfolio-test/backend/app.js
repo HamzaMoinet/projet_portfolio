088190ba65292a43366c4d418996aac780d9441c
HamzaMoinet <moinethamza0@gmail.com>
2025-06-23 14:12:23 +0200

    add cors and new route for commande user/admin

--- projet_portfolio-test/backend/app.js
+++ projet_portfolio-test/backend/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('./db/db');
 const isAdmin = require('./middleware/isAdmin');
 const auth = require('./middleware/auth');
-const cors = require('cors'); // Ajout de CORS
+const cors = require('cors');
 const { swaggerUi, swaggerSpec } = require('./swagger');
 
 const app = express();
-app.use(cors()); // Activation de CORS
+app.use(cors({
+  origin: 'http://localhost:5173' // Autorise le front Vite
+}));
 app.use(express.json());
 
 // Importer les routes
@@ -15,6 +17,8 @@ const menuRoutes = require('./routes/admin/menus');
 const ingredientRoutes = require('./routes/admin/ingredients');
 const platRoutes = require('./routes/admin/plats');
 const loginRoutes = require('./routes/login');
+const userCommandeRoutes = require('./routes/user/commandes');
+const adminCommandeRoutes = require('./routes/admin/commandes');
 
 app.use('/api/users', userRoutes);
 app.use('/api/login', loginRoutes);
@@ -22,6 +26,8 @@ app.use('/api/login', loginRoutes);
 app.use('/api/admin/menus', auth, isAdmin, menuRoutes);
 app.use('/api/admin/ingredients', auth, isAdmin, ingredientRoutes);
 app.use('/api/admin/plats', auth, isAdmin, platRoutes);
+app.use('/api/user/commandes', auth, userCommandeRoutes);
+app.use('/api/admin/commandes', auth, isAdmin, adminCommandeRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
